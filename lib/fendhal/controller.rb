736520004142e6e52cb407955afb4659e5775017
Russell <russell@russelldunphy.com>
2013-05-23 13:36:45 +0100

    Define actions in the controller

--- lib/fendhal/controller.rb
+++ lib/fendhal/controller.rb
@@ -1,4 +1,17 @@
 module Fendhal
   class Controller < ActionController::Base
+
+    def self.defines *actions
+      actions.each do |action|
+        define_method(action) { class_for(action).new(self).action }
+      end
+    end
+
+    private
+
+    def class_for(action)
+      self.class.const_get(action.to_s.camelcase)
+    end
+
   end
 end
--- spec/lib/fendhal/controller_spec.rb
+++ spec/lib/fendhal/controller_spec.rb
@@ -3,5 +3,29 @@ require 'spec_helper'
 module Fendhal
   describe Controller do
 
+    class TestController < Controller
+
+      class Index < Action
+
+        def action
+          "Here's the action!"
+        end
+
+      end
+
+      defines :index
+
+    end
+
+    let (:controller) { TestController.new }
+
+    it 'defines the specified actions' do
+      controller.should respond_to :index
+    end
+
+    it 'delegates to the respective classes for each action' do
+      controller.index.should eq "Here's the action!"
+    end
+
   end
 end
